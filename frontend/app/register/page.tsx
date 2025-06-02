b4f532232179718564eb292de0ef3f79959591ff
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-06-02 12:09:04 +0100

    add registration page layout and functionality with form inputs and social sign-up options

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,21 +1,93 @@
-import Image from "next/image";
+// import Image from "next/image";
 
-export default function RegisterAdmin(){
-   return (
-      <div>
-         <div>
-            <Image src="/images/image1.jpg" alt="School desks" />
-         </div>
-         <div>
-            <h1>Admin Sign Up</h1>
-            <div>
-               <div>Sign up with Google</div>
-               <div>Sign up with Apple</div>
+import Link from "next/link";
+
+export default function RegisterAdmin() {
+  return (
+    <div className="bg-[url('/images/bg-image.jpg')] min-h-screen bg-cover bg-no-repeat bg-black/70 bg-blend-overlay">
+      <div className="grid grid-cols-2 gap-10 px-44 pt-14">
+        <div className="text-white">
+          <div className="font-bold text-3xl pt-20 pb-20">Class Forge</div>
+          <div className="w-[400px]">
+            <h1 className="font-bold text-3xl">Management Meets Excellence.</h1>
+            <p className="font-light text-sm mt-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Corrupti, soluta. Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit
+            </p>
+          </div>
+        </div>
+        <div className="bg-white p-8 rounded-t-lg">
+          <h3 className="font-medium text-2xl mb-6">Create an Account</h3>
+          <form action="" className="flex flex-col gap-3 text-sm">
+            <div className="flex flex-col relative">
+              <label
+                htmlFor=""
+                className="bg-white text-gray-700 px-1 absolute left-4 -top-2 text-xs"
+              >
+                Your Name
+              </label>
+              <input
+                type="text"
+                className="border border-gray-300 text-gray-700 rounded-md px-3 py-2 focus:ring-1"
+                placeholder="Debo Kuda"
+              />
             </div>
-            <form action="">
+            <div className="flex flex-col relative">
+              <label
+                htmlFor=""
+                className="bg-white text-gray-700 px-1 absolute left-4 -top-2 text-xs"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                className="border border-gray-300 text-gray-700 rounded-md px-3 py-2 focus:ring-1"
+                placeholder="debokuda@gmail.com"
+              />
+            </div>
+            <div className="flex flex-col relative">
+              <label
+                htmlFor=""
+                className="bg-white text-gray-700 px-1 absolute left-4 -top-2 text-xs"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="border border-gray-300 text-gray-700 rounded-md px-3 py-2 focus:ring-1"
+                placeholder="**********"
+              />
+            </div>
 
-            </form>
-         </div>
+            <button className="bg-[#212121] rouned-md text-white text-sm font-bold py-3 rounded-md">
+              SIGN UP
+            </button>
+          </form>
+          <div className="flex items-center my-5">
+            <hr className="flex-grow border-gray-300" />
+            <p className="text-center px-3 text-gray-700">or</p>
+            <hr className="flex-grow border-gray-300" />
+          </div>
+          <div className="text-gray-700 flex flex-col gap-3 text-sm">
+            <button className="border border-gray-300 py-3 rounded-md">
+              Sign up with Google
+            </button>
+            <button className="border border-gray-300 py-3 rounded-md">
+              Sign up with Facebook
+            </button>
+            <button className="border border-gray-300 py-3 rounded-md">
+              Sign up with Apple
+            </button>
+          </div>
+          <p className="text-center mt-6 text-xs text-gray-500">
+            Already have an account?{" "}
+            <span className="underline underline-offset-1 font-bold text-gray-900">
+              <Link href={""}></Link>LOGIN HERE
+            </span>
+          </p>
+        </div>
       </div>
-   )
+    </div>
+  );
 }
