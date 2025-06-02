c6700d51e7d2ddc559ddb7b7ff12668c5e6b28e2
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-06-02 10:46:13 +0100

    initialize frontend structure with Next.js, Tailwind CSS, and essential configurations

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function RegisterAdmin(){
+   return (
+      <div>
+         <div>
+            <Image src="/images/image1.jpg" alt="School desks" />
+         </div>
+         <div>
+            <h1>Admin Sign Up</h1>
+            <div>
+               <div>Sign up with Google</div>
+               <div>Sign up with Apple</div>
+            </div>
+            <form action="">
+
+            </form>
+         </div>
+      </div>
+   )
+}
