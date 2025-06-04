401808c8af3199646f6fd9c8ac61d63fc3d50751
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-06-04 20:10:12 +0100

    feat: remove old AddCourse component and create new component structure

--- frontend/app/(dashboard)/add-course/page.tsx
+++
@@ -1,9 +0,0 @@
-// import Image from "next/image"
-
-export default function AddCourse(){
-   return(
-      <div className="flex flex-col items-center justify-center h-screen text-gray-600">
-       
-      </div>
-   )
-}
