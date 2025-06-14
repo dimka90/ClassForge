3255d3c5727abf3997ab3bc7dfa5bb3c719a60c1
dimka90 <dimkayilrit@gmail.com>
2025-06-14 21:48:24 +0100

    chore: remove .next from version control

--- frontend/.next/static/chunks/app_(dashboard)_exam-halls_page_tsx_049ec2a0._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_2cbc5c36._.js"
-],
-    source: "dynamic"
-});
